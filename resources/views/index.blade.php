2d9ae2dfd711b10f42ebbd15d81ea71ea1b3115f
StephenZahra <stephenzahra101@gmail.com>
2024-09-01 21:25:53 +0200

    Added bulma ui package, added all 3 panels

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,28 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Herald</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
         <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
     </head>
     <body>
-        <div id="request-panel">
-            @if(!empty($requests))
-                @foreach($requests as $request)
-                    @include('partials.request', ['requests' => $requests, 'colours', $colours])
-                @endforeach
-            @else
-                <p>You have no requests</p>
-            @endif
+        <div class="columns is-gapless">
+            <div id="request-panel" class="column is-2">
+                @if(!empty($requests))
+                    @foreach($requests as $request)
+                        @include('partials.request', ['requests' => $requests, 'colours', $colours])
+                    @endforeach
+                @else
+                    <p>You have no requests</p>
+                @endif
+            </div>
+            <div class="column is-flex is-flex-direction-column">
+                <div id="request-making-panel" class="column">
+
+                </div>
+                <div id="response-panel" class="column">
+
+                </div>
+            </div>
         </div>
     </body>
 </html>
